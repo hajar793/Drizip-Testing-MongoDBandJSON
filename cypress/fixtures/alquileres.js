6bf3b7d57d3ff4c410e50accf58eab4d5910ee80
hajar793 <106870534+hajar793@users.noreply.github.com>
2022-06-23 01:06:44 +0300

    Update alquileres.js

---
+++ cypress/fixtures/alquileres.js
@@ -0,0 +1,87 @@
+module.exports=[
+    {
+        "_id": "segqPpzvzRMdLWjJK",
+        "fecha": {
+          "$date": "2022-06-21T19:38:06.015Z"
+        },
+        "fechaDesde": {
+          "$date": "2022-07-02T06:00:00Z"
+        },
+        "fechaHasta": {
+          "$date": "2022-07-09T06:00:00Z"
+        },
+        "solicitudes": [
+          {
+            "vehiculoId": "E4yYNimkRAWCqdEpF",
+            "preciodiario": 36526,
+            "precioestimado": 45006,
+            "comision": 0.25,
+            "seguromx": "NE"
+          }
+        ],
+        "pais": "MX",
+        "estado": "Awaiting",
+        "notificationAttempts": 1,
+        "timeZoneId": "America/Mexico_City",
+        "paymentGateway": "PaymentOS",
+        "conductorId": "GAJ76PZJsaXeYJc5S",
+        "created_at": {
+          "$date": "2022-06-21T19:38:05.111Z"
+        },
+        "historialEstados": [
+          {
+            "estado": "Awaiting",
+            "fecha": {
+              "$date": "2022-06-21T19:38:04.753Z"
+            }
+          }
+        ]
+      }
+      ,
+    [
+      
+      
+      {
+        "_id": "cm7gogCyqvTkFboBf",
+        "fecha": {
+          "$date": "2022-06-22T10:41:17.791Z"
+        },
+        "fechaDesde": {
+          "$date": "2022-07-11T07:00:00Z"
+        },
+        "fechaHasta": {
+          "$date": "2022-07-13T07:00:00Z"
+        },
+        "solicitudes": [
+          {
+            "vehiculoId": "PZLzJYHRExSNrBKWr",
+            "preciodiario": 1614,
+            "precioestimado": 11327,
+            "comision": 0.25
+          }
+        ],
+        "pais": "CL",
+        "estado": "Awaiting",
+        "notificationAttempts": 1,
+        "timeZoneId": "America/Santiago",
+        "paymentGateway": "PaymentOS",
+        "conductorId": "GAJ76PZJsaXeYJc5S",
+        "seguro": {
+          "plan": 2
+        },
+        "created_at": {
+          "$date": "2022-06-22T10:41:16.856Z"
+        },
+        "historialEstados": [
+          {
+            "estado": "Awaiting",
+            "fecha": {
+              "$date": "2022-06-22T10:41:16.619Z"
+            }
+          }
+        ]
+      }
+      
+    ]
+
+]
